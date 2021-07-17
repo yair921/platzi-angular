144a52d65cb6912bc9d23c682071cd17d7096812
Yair Montes <yair921@gmail.com>
2021-07-17 12:41:52 -0500

    partial advances in components lifeCycle

--- src/app/components/products.components.ts
+++ src/app/components/products.components.ts
@@ -1,13 +1,22 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChange } from '@angular/core'
 import { Product } from '../product.model'
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnChanges {
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('Constructor!')
+    }
+
+    ngOnChanges(changes: any) {
+        console.log('ngOnChange!')
+        console.log(changes)
+    }
 
     addCart() {
         console.log('Añadir al carrito!');
